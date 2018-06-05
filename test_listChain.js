08c890ea59d327916f17d9b2f17c96a62c3b5959
kenokabe <kenokabe@gmail.com>
2018-06-05 23:54:42 +0900

    commit

--- test_listChain.js
+++ test_listChain.js
@@ -5,16 +5,18 @@
 
   const _M = () => freeMonoid(operator);
   const operator = list => {
+    const composition = (f, g) => (x => g(f(x)));
     list.eval = () => list.val.reduce(composition); //lazy eval
+
     list.chain = (f) => !f.M
       ? list.val.reduce(reduction(f), (list.M))
       : list.val.reduce(reduction(f.eval()), (list.M));
-    list.reduce = (f) => (list.M)(f(list.val));
     const reduction = (f) => (m, x) => (m)(f(x));
-    const composition = (f, g) => (x => g(f(x)));
+
+    list.reduce = (f) => (list.M)(f(list.val));
+
   };
   const M = _M();
-
 
   const mlog = m => o => {
     console.log(m + "\n" + o.val);
@@ -30,12 +32,13 @@
     xyz
   );
 
+  const double = (a) => (M)(a)(a);
   const add1 = (M)((a) => (a + 1));
 
   mlog("xyz--chain------")(
     xyz
-      .chain((a) => (M)(a)(a))
-      .chain((a) => (M)(a)(a))
+      .chain(double)
+      .chain(double)
       .chain((add1)(add1))
       .chain(add1)
   );
@@ -46,5 +49,45 @@
   mlog("------")(
     (add1)(add1).chain(f => f(3)) //4,4
   );
+
+  mlog("------")(
+    (M)(9).chain(x => x)
+  );
+  console.log("------");
+
+  const plus = (x) => (y => x + y);
+  console.log(
+    plus(1)(5)
+  );
+  mlog("------")(
+    (M)(1).chain(plus(5))
+  );
+  mlog("------")(
+    (M)(plus).chain(plus)
+  );
+
+  const plus1 = (M)(1).chain(plus);
+  mlog("------")(
+    M(5)(plus1)
+      .chain(plus1)
+  );
+  mlog("------")(
+    (plus1)
+      .chain(plus1)
+  );
+  mlog("------")(
+    (plus1)(plus1)
+  );
+
+  mlog("------")(
+    (plus1)(5)
+  );
+  mlog("------")(
+    M(5).chain(plus1)
+  );
+
+
+
+
 
 })();
