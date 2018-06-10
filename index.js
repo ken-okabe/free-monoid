ceec09f21e015a0c00ad508b6449b85e9148f263
kenokabe <kenokabe@gmail.com>
2018-06-10 13:50:59 +0900

    commit

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 (() => {
   "use strict";
-  const freeMonoid = (operator) => {
-    const M = (m) => (!!m && (!!m.M || m.identity))
-      ? (m)
-      : (() => {
-        const a = b => (b.identity) //M
-          ? (a)
-          : !b.M
-            ? (a)(M(b))
-            : (() => {
-              const ab = M();
-              ab.units = a.units.concat(b.units);
-              ab.val = ab.units.map(unit => unit.val[0]);
-              return ab; // (a)(b)
-            })();
-        a.val = a.val ? [] : [m];
-        a.units = [a];
-        a.M = (m) => M(m);
-        a.M.identity = true;
-        a.M.val = (m) => (m);
-        operator(a);
-        return a;
-      })();
-    M.identity = true;
-    M.val = (m) => (m);
+  const freeMonoid = (operator) => (() => {
+    const M = (() => { //(M)(a)(b)
+      const m = (a) => (!!a && (!!a.M || a.identity))
+        ? (a)
+        : (() => {
+          const ma = b => (b.identity) //M
+            ? (ma)
+            : !b.M
+              ? (ma)(M(b))
+              : (() => {
+                const mab = M();
+                const [m, ...bUnits] = b.units;
+                mab.units = ma.units.concat(bUnits);
+                mab.val = mab.units.map(unit => unit.val[0]);
+                return mab; // (m)(a)(b)
+              })();
+          ma.val = [a];
+          ma.M = m;
+          ma.units = m.units.concat(ma);
+          operator(a);
+          return ma;
+        })();
+      m.identity = true;
+      m.val = ["__IDENTITY__"];
+      m.units = [m];
+      return m;
+    })();
     return M;
-  };
-  //------------------
+  })();
+  //=================
   const exporting = (typeof module === "object"
   && typeof module.exports === "object")
     ? module.exports = freeMonoid
     : self.freeMonoid = freeMonoid;
-//============================
+    //============================
+
 })();
--- test.js
+++ test.js
@@ -4,24 +4,71 @@
   const freeMonoid = require("./index");
   const _M = () => freeMonoid(operator);
   const operator = list => {
-    list.eval = () => list.val.reduce((a, b) => (a + b));
+
+    //const M = list.M;
+
+    //list.toArr = list.units.map(unit =>);
+
   };
   const M = _M();
+  //---------------------------
 
-  const x = M(1);
-  const y = M(2);
-  const z = M(5);
 
-  console.log(x);
   console.log(
-    (M)(x) // === (x) left identity
+    "============================================"
   );
   console.log(
-    (x)(M) // === (x) right identity
+    (M)
   );
 
-  const xyz = (x)(y)(z);
-  console.log(xyz);
-  console.log(xyz.eval()); //lazy eval
+  console.log(
+    "============================================"
+  );
+  console.log(
+    (M)()
+  );
+  console.log(
+    "============================================"
+  );
+  console.log(
+    (M)(1)
+  );
+  console.log(
+    "============================================"
+  );
+  console.log(
+    (M)(1)(M)
+  );
+  console.log(
+    "============================================"
+  );
+  console.log(
+    (M)(1)(2)
+  );
+  console.log(
+    "============================================"
+  );
+  console.log(
+    (M)(1)(2)(9)
+  );
+
+  const ab = (M)(1)(2);
+
+  const cd = (M)(10)(20);
+
+  const e = (M)(9);
+
+  const ecd = (e)(cd);
+
+  const abecd = (ab)(ecd);
+
+
+  console.log(
+    "============================================"
+  );
+  console.log(
+    abecd
+  );
+
 
 })();
